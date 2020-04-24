32a649828cbadc93b291bba530d1dc6e9516d232
Benjamin Thom <jaminthom@gmail.com>
2020-04-24 10:19:56 -0700

    add src/TapList.js

---
+++ src/TapList.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Tap from './Tap';
+
+const masterTapList = [
+  {
+    name: "Green Monster",
+    brand: "Portland Brewers Guild",
+    price: 8,
+    style: "Stout",
+    quantity: 124
+  },
+  {
+    name: "Hoppin' Out",
+    brand: "Portland Brewers Guild",
+    price: 9,
+    style: "Pale Ale",
+    quantity: 124
+  },
+  {
+    name: "Sour Squeeze",
+    brand: "Portland Brewers Guild",
+    price: 8,
+    style: "Sour",
+    quantity: 124
+  }
+];
+
+function TapList(){
+  return (
+    <React.Fragment>
+      {masterTapList.map((tap, index) =>
+        <Tap
+          name={tap.name}
+          brand={tap.name}
+          price={tap.price}
+          style={tap.style}
+          quantity={tap.quantity}/>
+      )}
+    </React.Fragment>
+  );
+}
+
+export default TapList;
