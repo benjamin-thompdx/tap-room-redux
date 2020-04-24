d0455b12ed8ec8ed877ea1103082c16824b88827
Benjamin Thom <jaminthom@gmail.com>
2020-04-24 14:45:09 -0700

    update src/Tap.js to include the whenTapClicked prop from TapList component's OnTapSelection method

--- src/Tap.js
+++ src/Tap.js
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types';
 function Tap(props){
   return (
     <React.Fragment>
-      <p><strong>Name:</strong> <em>{props.name}</em></p>
-      <p><strong>Brand:</strong> <em>{props.brand}</em></p>
-      <p><strong>Price:</strong> <em>${props.price}</em></p>
-      <p><strong>Style:</strong> <em>{props.style}</em></p>
-      <p><strong>Quantity:</strong> <em>{props.quantity} pints</em></p>
-      <button type="submit">Sell</button>
+      <div onClick = {() =>  props.whenTapClicked(props.id)}>
+        <p><strong>Name:</strong> <em>{props.name}</em></p>
+        <p><strong>Brand:</strong> <em>{props.brand}</em></p>
+        <p><strong>Price:</strong> <em>${props.price}</em></p>
+        <p><strong>Style:</strong> <em>{props.style}</em></p>
+        <p><strong>Quantity:</strong> <em>{props.quantity} pints</em></p>
+        <button type="submit">Sell</button>
+      </div>
     </React.Fragment>
-  )
+  );
 }
 
 Tap.propTypes = {
@@ -19,7 +21,9 @@ Tap.propTypes = {
   brand: PropTypes.string,
   price: PropTypes.number,
   style: PropTypes.string,
-  quantity: PropTypes.number
+  quantity: PropTypes.number,
+  id: PropTypes.string,
+  whenTapClicked: PropTypes.func
 }
 
 export default Tap;
