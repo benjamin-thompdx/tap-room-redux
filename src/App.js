8f915e3dc0367ee845847d4977ed0ed10ade1c60
Benjamin Thom <jaminthom@gmail.com>
2020-04-24 10:26:41 -0700

    update project to include stateless components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import TapList from './TapList';
 import './App.css';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <TapList />
     </React.Fragment>
   );
 }
