896ffac22d67b16b2c9ee31ecb576a224735e60b
Benjamin Thom <jaminthom@gmail.com>
2020-04-24 13:35:08 -0700

    update src/TapControl.js to include an event handler method to handle form submissions

--- src/TapControl.js
+++ src/TapControl.js
@@ -18,6 +18,14 @@ class TapControl extends React.Component {
     }));
   }
 
+  handleAddingNewTapToList = (newTap) => {
+    const newMasterTapList = this.state.masterTapList.concat(newTap);
+    this.setState({
+      masterTapList: newMasterTapList,
+      formVisibleOnPage: false
+    });
+  }
+
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
