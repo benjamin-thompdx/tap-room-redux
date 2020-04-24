80abe48d221e2c488a0a83c009b76aa148e0b54a
Benjamin Thom <jaminthom@gmail.com>
2020-04-24 11:20:16 -0700

    implement conditional rendering within src/TapControl.js component

--- src/TapControl.js
+++ src/TapControl.js
@@ -12,9 +12,15 @@ class TapControl extends React.Component {
   }
 
   rendor(){
+    let currentlyVisibleState = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTapForm />
+    } else {
+      currentlyVisibleState = <TapList />
+    }
     return (
       <React.Fragment>
-
+        {currentlyVisibleState}
       </React.Fragment>
     );
   }
