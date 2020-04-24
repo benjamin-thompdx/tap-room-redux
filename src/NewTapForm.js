4e10f9b82a625126be783e27b65fdc57365041f7
Benjamin Thom <jaminthom@gmail.com>
2020-04-24 13:10:36 -0700

    define the event handler method for NewTapFormSubmission within the src/NewTapForm.js component

--- src/NewTapForm.js
+++ src/NewTapForm.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import { v4 } from 'uuid';
 
-function NewTabList(props){
+function NewTapForm(){
+
+  function handleNewTapFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(event.target.brand.value);
+    console.log(event.target.price.value);
+    console.log(event.target.style.value);
+    console.log(event.target.quantity.value);
+  }
+
   return (
     <React.Fragment>
-      <form>
+      <form onSubmit={handleNewTapFormSubmission}>
         <h3>New Tap Form</h3>
+        <input 
+          type='text'
+          name='name'
+          placeholder='tap name' />
+        <input 
+          type='text'
+          name='brand'
+          placeholder='tap brand' />
+        <input 
+          type='number'
+          name='price'
+          placeholder='tap price' />
+        <input 
+          type='text'
+          name='style'
+          placeholder='tap style' />
+        <input 
+          type='number'
+          name='quantity'
+          placeholder='tap quantity (in pints)' />
+        <button type='submit'>add tap</button>
       </form>
     </React.Fragment>
   );
 }
 
-export default NewTabList;
+export default NewTapForm;
