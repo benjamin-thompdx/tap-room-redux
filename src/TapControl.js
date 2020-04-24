ef72a476de44abf2c7e1fcb9afbea4a0729686ef
Benjamin Thom <jaminthom@gmail.com>
2020-04-24 11:07:10 -0700

    add src/TapControl.js

---
+++ src/TapControl.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import NewTapForm from './NewTapForm';
+import TapList from './TapList';
+
+class TapControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  rendor(){
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+export default TapControl;
