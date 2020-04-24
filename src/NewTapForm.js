b85243b4d2d7f2bb9827bf8381a39cea03dcfa97
Benjamin Thom <jaminthom@gmail.com>
2020-04-24 10:52:00 -0700

    add src/NewTapForm.js

---
+++ src/NewTapForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NewTabList(props){
+  return (
+    <React.Fragment>
+      <form>
+        <h3>New Tap Form</h3>
+      </form>
+    </React.Fragment>
+  );
+}
+
+export default NewTabList;
