a3f33b6ac9d849d48c2018e073e0b4fa9794a1e6
Benjamin Thom <jaminthom@gmail.com>
2020-04-24 14:58:19 -0700

    update rc/TapDetail.js to render selectedTap by passing down the tap prop using object destructuring

--- src/TapDetail.js
+++ src/TapDetail.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function TapDetail(props){
+  const { tap } = props;
+
   return (
     <React.Fragment>
       <h1>Tap Details</h1>
+      <p><strong>Name:</strong> <em>{props.name}</em></p>
+      <p><strong>Brand:</strong> <em>{props.brand}</em></p>
+      <p><strong>Price:</strong> <em>${props.price}</em></p>
+      <p><strong>Style:</strong> <em>{props.style}</em></p>
+      <p><strong>Quantity:</strong> <em>{props.quantity} pints</em></p>
     </React.Fragment>
   );
 }
+
+TapDetail.propTypes = {
+  tap: PropTypes.object
+};
 
 export default TapDetail;
