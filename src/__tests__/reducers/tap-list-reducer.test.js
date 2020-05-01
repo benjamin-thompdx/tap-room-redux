c477ad9dcfd0ba6a6712637d143d77c8fde9f8af
Benjamin Thom <jaminthom@gmail.com>
2020-05-01 10:07:16 -0700

    update src/__tests__/reducers/tap-list-reducer.test.js to declare an action,include tapData constant, and write  our second test. successfully fail test

--- src/__tests__/reducers/tap-list-reducer.test.js
+++ src/__tests__/reducers/tap-list-reducer.test.js
@@ -1,7 +1,43 @@
 import tapListReducer from '../../reducers/tap-list-reducer';
 
 describe('tapListReducer', () => {
+
+  let action;
+  const tapData = {
+    name: 'Mountain Water',
+    brand: 'Butte Brewery',
+    price: 8,
+    style: 'Blonde Ale',
+    quantity: 124,
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(tapListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new tap data to masterTapList', () => {
+    const { name, brand, price, style, quantity, id } = tapData;
+    action = {
+      type: 'ADD_TAP',
+      name: name,
+      brand: brand,
+      price: price,
+      style: style,
+      quantity: quantity,
+      id: id
+    };
+
+    expect(tapListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        style: style,
+        quantity: quantity,
+        id: id
+      }
+    });
+  });
+
 });
