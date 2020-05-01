7b77b6d00868698b60d1d848355686e6212b2c3c
Benjamin Thom <jaminthom@gmail.com>
2020-05-01 10:15:16 -0700

    update src/reducers/tap-list-reducer.js to include logic to successfully pass our second test. pass second test.

--- src/reducers/tap-list-reducer.js
+++ src/reducers/tap-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_TAP':
+      const { name, brand, price, style, quantity, id } = action;
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          price: price,
+          style: style,
+          quantity: quantity,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 };
