a1c9b5e7c8b8bcc6c7ed928ef845dda57219836b
Benjamin Thom <jaminthom@gmail.com>
2020-05-01 10:50:27 -0700

    update src/index.js to include React-Redux provider configuration

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import App from './components/App';
 import { createStore } from 'redux';
 import reducer from './reducers/tap-list-reducer';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
