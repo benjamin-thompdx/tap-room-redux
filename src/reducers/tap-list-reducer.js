9dac088a3c479b9bb7865ed56099f5c3925d555b
Benjamin Thom <jaminthom@gmail.com>
2020-05-01 18:11:34 -0700

    update src/reducers/tap-list-reducer.js to include SELL_PINT case, update properties syntax to resolve scope issues

--- src/reducers/tap-list-reducer.js
+++ src/reducers/tap-list-reducer.js
@@ -3,15 +3,25 @@ import * as c from './../actions/ActionTypes';
 export default (state = {}, action) => {
   switch (action.type) {
     case c.ADD_TAP:
-      const { name, brand, price, style, quantity, id } = action;
       return Object.assign({}, state, {
-        [id]: {
-          name: name,
-          brand: brand,
-          price: price,
-          style: style,
-          quantity: quantity,
-          id: id
+        [action.id]: {
+          name: action.name,
+          brand: action.brand,
+          price: action.price,
+          style: action.style,
+          quantity: action.quantity,
+          id: action.id
+        }
+      });
+    case c.SELL_PINT:
+      return Object.assign({}, state, {
+        [action.id]: {
+          name: action.name,
+          brand: action.brand,
+          price: action.price,
+          style: action.style,
+          quantity: action.quantity,
+          id: action.id
         }
       });
     default:
